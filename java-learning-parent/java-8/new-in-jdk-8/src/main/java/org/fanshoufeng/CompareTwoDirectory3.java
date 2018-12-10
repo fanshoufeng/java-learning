4dd3af65db4ce4544f242d067669caf5af046885
Michael Fan <shoufeng.fan@gmail.com>
2018-12-10 13:47:26 +0800

    打印信息调整

--- java-learning-parent/java-8/new-in-jdk-8/src/main/java/org/fanshoufeng/CompareTwoDirectory3.java
+++ java-learning-parent/java-8/new-in-jdk-8/src/main/java/org/fanshoufeng/CompareTwoDirectory3.java
@@ -8,14 +8,15 @@ import java.nio.file.Paths;
 
 public class CompareTwoDirectory3 {
 
-	private static void compareAndPrint(String directory1, String directory2) {
+	private static void compareAndPrint(String directory1, String directory2, String printMsg) {
+		System.out.println(printMsg);
 		try {
 			Files.walk(Paths.get(directory1), FileVisitOption.FOLLOW_LINKS)
 				.filter(path -> path.toString().endsWith(".java"))
-				.map(path -> path.toString())
-				.filter(path -> Files.notExists(Paths.get(path.replace(directory1, directory2)), LinkOption.NOFOLLOW_LINKS))
+				.map(path -> path.toString().replace(directory1 + "\\", ""))
+				.filter(path -> Files.notExists(Paths.get(directory2, path), LinkOption.NOFOLLOW_LINKS))
 				.sorted((path1, path2) -> path1.compareTo(path2))
-				.forEach(path -> System.out.println(path.replace(directory1 + "\\", "")));
+				.forEach(System.out::println);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -27,10 +28,8 @@ public class CompareTwoDirectory3 {
 	public static void main(String[] args) {
 		String directory1 = "E:\\Source Code\\Java\\jdk1.7.0_79\\src";
 		String directory2 = "E:\\Source Code\\Java\\jdk1.8.0_171\\src";
-		System.out.println("删除的文件有:");
-		compareAndPrint(directory1, directory2);
-		System.out.println("\n添加的文件有:");
-		compareAndPrint(directory2, directory1);
+		compareAndPrint(directory1, directory2, "删除的文件有:");
+		compareAndPrint(directory2, directory1, "\n添加的文件有:");
 	}
 
 }
